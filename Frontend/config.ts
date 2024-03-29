33a77bce03a8bcd1d502aabfef809d870aba38fe
AndreyPetrozhitskiy <super.okha2018@yandex.ru>
2024-03-29 15:43:32 +1000

    finish dev

--- Frontend/config.ts
+++ Frontend/config.ts
@@ -4,7 +4,7 @@ const PROTOCOL:string = "http"
 // порт backend
 const PORT:number = 2024
 // ip backend
-const URL:string = "localhost"
+const URL:string = "74.119.192.138"
 
 
 export const API_BASE_URL = `${PROTOCOL}://${URL}:${PORT}`;
